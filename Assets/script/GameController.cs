649e1aff074378424e6808e255c377faa1bdc9ff
liengpro123 <lienguangpro@gmail.com>
2023-06-20 08:23:40 +0900

    removed Assets

--- Assets/script/GameController.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameController : MonoBehaviour
-{
-    public static CameraShake camShake;
-    public static bool isGameAlive = true;
-}
